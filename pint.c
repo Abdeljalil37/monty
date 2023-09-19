ae8c2b2fda0152df4593498d6f9435d37f62d7ba
shixyman <shixymanflowx@gmail.com>
2023-09-19 09:12:36 -0700

    modify

--- pint.c
+++ pint.c
@@ -7,11 +7,11 @@
  * Return: no return nothing
 */
 
-void f_pint(stack_t **head, unsigned int count)
+void f_pint(stack_t **head, unsigned int counter)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
