eff8be200ab84d905c69917a2e326d8d315d13ad
Abdeljalil37 <widadpop@gmail.com>
2023-09-19 16:49:02 +0100

    header file

---
+++ monty.h
@@ -0,0 +1,13 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+
+#endif
