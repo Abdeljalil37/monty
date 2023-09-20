2f0689cc9ed84254eed799a81667b711628ac083
Abdeljalil37 <widadpop@gmail.com>
2023-09-20 18:44:02 +0100

    stack struct

--- monty.h
+++ monty.h
@@ -8,6 +8,22 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <ctype.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
 
 /**
  * struct bus_s - variables -args, file, line content
